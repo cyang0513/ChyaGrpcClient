2875c0a832a0189f4f2d26d6577ea3d86a5140e7
Chengkai Yang <yangchengkai@gmail.com>
2020-12-30 16:41:23 +0100

    Client streaming call

--- Program.cs
+++ Program.cs
@@ -30,24 +30,49 @@ namespace ChyaGrpcClient
          {
             Input = inputKey
          };
-         var res = client.GetEcho(input);    
+         var res = client.GetEcho(input);
          Console.WriteLine("Get echo: " + res.Output.Trim());
 
-         Console.WriteLine("Type in something to get echo stream (Server stream call): ");
+         Console.WriteLine("Type in something to get echo stream (Server streaming call): ");
          inputKey = Console.ReadLine();
          input = new EchoInput()
          {
             Input = inputKey
          };
 
-         var task = HandleEchoStreamCallAsync(client, input);
+         var task = HandleServerStreamingCallAsync(client, input);
          task.Wait();
+
+         Console.WriteLine("Type 3 line to get a echo return (Client streaming call): ");
+         HandleClientStreamingCall(client);
 
          Console.ReadKey();
 
       }
 
-      public static async Task HandleEchoStreamCallAsync(TestService.TestServiceClient client, EchoInput input)
+      private static void HandleClientStreamingCall(TestService.TestServiceClient client)
+      {
+         var call = client.GetInputStream();
+
+         for (int i = 0; i < 3; i++)
+         {
+            var inputLine = Console.ReadLine();
+
+            call.RequestStream.WriteAsync(new EchoInput()
+            {
+               Input = inputLine
+            });
+         }
+
+         call.RequestStream.CompleteAsync();
+         var response = call.ResponseAsync;
+         response.Wait();
+
+         Console.WriteLine("Get input from stream: " + Environment.NewLine + response.Result.Output);
+         Console.WriteLine("Get time: " + response.Result.TimeStamp.ToDateTime().ToLocalTime());
+      }
+
+      public static async Task HandleServerStreamingCallAsync(TestService.TestServiceClient client, EchoInput input)
       {
          var resStream = client.GetEchoStream(input);
 
